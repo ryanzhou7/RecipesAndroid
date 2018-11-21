42619823c0919f31b96b0442d52ab9dc3c0b4fb2
Ryan Zhou <ryanzhou7@gmail.com>
2018-11-21 11:04:36 -0800

    Add uncommitted files

--- app/src/main/java/com/example/ryanzhouold/bakingandroid/MainActivity.java
+++ app/src/main/java/com/example/ryanzhouold/bakingandroid/MainActivity.java
@@ -1,60 +1,31 @@
 package com.example.ryanzhouold.bakingandroid;
 
-import android.net.Uri;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.ryanzhouold.bakingandroid.RecipeDetail.RecipeStepFragment;
-import com.example.ryanzhouold.bakingandroid.RecipeList.RecipeListFragment;
+import com.example.ryanzhouold.bakingandroid.RecipeList.RecipeFragment;
 import com.example.ryanzhouold.bakingandroid.modelLayer.pojo.Recipe;
 
-public class MainActivity extends AppCompatActivity implements RecipeListFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements RecipeFragment.OnListFragmentInteractionListener{
 
-    private RecipeListFragment mRecipeListFragment;
-    private RecipeStepFragment mRecipeStepFragment;
-    private boolean mTwoPane;
+    private RecipeFragment mRecipeListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTwoPane = getResources().getBoolean(R.bool.isTabletOrLarger);
-        if(mTwoPane){
-            mRecipeListFragment = new RecipeListFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.left_container, mRecipeListFragment).commit();
-        }
-        else{
-            mRecipeListFragment = new RecipeListFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.recipe_list_container, mRecipeListFragment).commit();
-        }
-    }
-
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    @Override
-    public void onRecipesLoaded(Recipe recipe) {
-        if(mTwoPane){
-            mRecipeStepFragment = RecipeStepFragment.newInstance("", recipe);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.right_container, mRecipeStepFragment)
-                    .commit();
-        }
-    }
-
-    @Override
-    public void onRecipeClicked(Recipe recipe) {
-        mRecipeStepFragment = RecipeStepFragment.newInstance("", recipe);
+        mRecipeListFragment = RecipeFragment.newInstance(1);
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.recipe_list_container, mRecipeStepFragment)
-                .setTransitionStyle(FragmentTransaction.TRANSIT_ENTER_MASK)
-                .addToBackStack(RecipeStepFragment.class.getSimpleName())
-                .commit();
+                .add(R.id.main_container, mRecipeListFragment).commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(Recipe item) {
+        /*
+        Recipe recipe = mRecipeAdapter.getRecipes().get(index);
+        Intent detailedRecipeIntent = new Intent(getActivity(), RecipeDetailActivity.class);
+        detailedRecipeIntent.putExtra(RECIPE_KEY, recipe);
+        startActivity(detailedRecipeIntent);
+        */
     }
 }
