a71531fc132b5e909d3f46584502f74418e3c651
Ryan Zhou <ryanzhou7@gmail.com>
2019-03-12 13:21:32 -0700

    Add timber logging lib, fix retrofit network call

--- app/src/main/java/com/example/ryanzhouold/bakingandroid/data/repository/BaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ryanzhouold.bakingandroid.data.repository;
-
-import com.example.ryanzhouold.bakingandroid.data.database.AppDatabase;
-
-public abstract class BaseRepository{
-    protected AppDatabase mAppDatabase;
-}
